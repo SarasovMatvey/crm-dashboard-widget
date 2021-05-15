fbe219c80df8a9b50d4b836243f820b80d33a31e
SarasovMatvey <sarasovmatvej@gmail.com>
2021-05-15 17:23:05 +0600

    added base structure

--- src/js/_blocks/_crm-info-widget.js
+++ src/js/_blocks/_crm-info-widget.js
@@ -1,17 +1,70 @@
-import jss from "../node_modules/jss";
+class CrmWidget {
+  #widget = document.createElement('div');
 
-(() => {
-  const widget = document.querySelector(".crm-info-widget");
+  init() {
+    this.#widget.classList.add('crm-info-widget')
+    this.#widget.innerHTML = this.#widgetBody;
+    document.body.prepend(this.#widget);
+    this.#setStyles();
 
-  const styles = {
-    toggle: `
-      background: red;
-    `,
-  };
+    this.#widget.querySelector('.crm-info-widget__toggle').addEventListener('click', this.#toggleClickHandler.bind(this));
+    const tabs = this.#widget.querySelectorAll('.crm-info-widget__tab-title')
+    for (const tab of tabs) {
+      tab.addEventListener('click', this.#tabClickHandler.bind(this))
+    }
+  }
 
-  const { classes } = jss.createStyleSheet(styles).attach();
+  #toggleClickHandler() {
+    this.#widget.querySelector('.crm-info-widget__toggle').classList.toggle('crm-info-widget__toggle_active');
+    this.#widget.querySelector('.crm-info-widget__info-panel').classList.toggle('crm-info-widget__info-panel_opened');
+    this.#widget.querySelector('.crm-info-widget__backdrop').classList.toggle('crm-info-widget__backdrop_visible');
+  }
 
-  widget.innerHTML = `
-    <div className="${classes.toggle}">dashboard</div>
-  `;
-})();
+  #tabClickHandler(e) {
+    const tabs = this.#widget.querySelectorAll('.crm-info-widget__tab-body');
+    const selectedTab = e.currentTarget.parentNode.querySelector('.crm-info-widget__tab-body');
+
+    for (const tab of tabs) {
+      if (tab !== selectedTab) {
+        tab.classList.remove('crm-info-widget__tab-body_active');
+      }
+    }
+    selectedTab.classList.toggle('crm-info-widget__tab-body_active');
+  }
+
+  #css(selector, styles) {
+    this.#widget.querySelector(selector).setAttribute('style', styles);
+  }
+
+  #setStyles() {
+    this.#css('.crm-info-widget__toggle', `
+    `);
+  }
+
+  
+  #widgetBody = `
+    <button class="crm-info-widget__toggle">CRM</button>
+    <aside class="crm-info-widget__info-panel">
+      <ul class="crm-info-widget__tabs">
+        <li class="crm-info-widget__tab">
+          <h2 class="crm-info-widget__tab-title">Онлайн-запись</h2>
+          <div class="crm-info-widget__tab-body">Hello how are you)</div>
+        </li>
+        <li class="crm-info-widget__tab">
+          <h2 class="crm-info-widget__tab-title">История визитов</h2>
+          <div class="crm-info-widget__tab-body">Hello how are you)</div>
+        </li>
+        <li class="crm-info-widget__tab">
+          <h2 class="crm-info-widget__tab-title">Файлы</h2>
+          <div class="crm-info-widget__tab-body">Hello how are you)</div>
+        </li>
+        <li class="crm-info-widget__tab">
+          <h2 class="crm-info-widget__tab-title">Оплаты</h2>
+          <div class="crm-info-widget__tab-body">Hello how are you)</div>
+        </li>
+      </ul>
+    </aside>
+    <div class="crm-info-widget__backdrop"></div>
+  `
+
+}
